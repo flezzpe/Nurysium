48e15aef0270e85181fda2fb9fa4b878c5fd4959
flezzpe <98888240+flezzpe@users.noreply.github.com>
2024-07-05 07:14:31 +0700

    Update notify_UI.lua

--- notify_UI.lua
+++ notify_UI.lua
@@ -1,159 +1,128 @@
-local notify = {}
+local TweenService = game:GetService('TweenService')
+local TextService = game:GetService('TextService')
+local Debris = game:GetService('Debris')
 
-local TweenService = game:GetService("TweenService")
+local notify_lib = {
+	notify = Instance.new("ScreenGui"),
+	notify_count = 0
+}
 
-local notify_List = nil
+function notify_lib.init()
+	notify_lib.notify.Name = "notify"
+	notify_lib.notify.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
 
-function notify.init(parent: any)
-	local self = Instance.new("ScreenGui", parent)
-	
-	notify_List = Instance.new("Frame")
+	local notify_list = Instance.new("Frame")
 	local UIListLayout = Instance.new("UIListLayout")
 
-	notify_List.Name = "notify_List"
-	notify_List.Parent = self
-	notify_List.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
-	notify_List.BackgroundTransparency = 1.000
-	notify_List.BorderColor3 = Color3.fromRGB(0, 0, 0)
-	notify_List.BorderSizePixel = 0
-	notify_List.Position = UDim2.new(0.388183862, 0, 0.537472963, 0)
-	notify_List.Size = UDim2.new(0.223632291, 0, 0.40625, 0)
-
-	UIListLayout.Parent = notify_List
+	notify_list.Name = "notify_list"
+	notify_list.Parent = notify_lib.notify
+	notify_list.AnchorPoint = Vector2.new(0.5, 0.5)
+	notify_list.BackgroundColor3 = Color3.fromRGB(191, 229, 248)
+	notify_list.BackgroundTransparency = 1.000
+	notify_list.BorderColor3 = Color3.fromRGB(0, 0, 0)
+	notify_list.BorderSizePixel = 0
+	notify_list.Position = UDim2.new(0.5, 0, 0.8, 0)
+	notify_list.Size = UDim2.new(0, 495, 0, 440)
+	
+	UIListLayout.Parent = notify_list
 	UIListLayout.HorizontalAlignment = Enum.HorizontalAlignment.Center
 	UIListLayout.SortOrder = Enum.SortOrder.LayoutOrder
-	UIListLayout.Padding = UDim.new(0.0149999997, 0)
+	UIListLayout.Padding = UDim.new(0.015, 0)
 end
 
-function notify.render_Notify(notify_Text: string, on_screen_Time: number)
-	task.defer(function()
+function notify_lib.draw_notify(text: string, draw_time: number)
+	notify_lib.notify_count += 1
+
+	if notify_lib.notify_count > 5 then
+		for _, old_notify in notify_lib.notify.notify_list:GetChildren() do
+			if old_notify:IsA('Frame') then
+				Debris:AddItem(old_notify, 0)
+
+				task.wait()
+			end
+		end
 		
-		local Example = Instance.new("Frame")
-		local UICorner = Instance.new("UICorner")
-		local Data = Instance.new("Folder")
-		local Icon = Instance.new("ImageLabel")
-		local Text = Instance.new("TextLabel")
-		local UIGradient = Instance.new("UIGradient")
-		local Line = Instance.new("Frame")
-		local UIAspectRatioConstraint = Instance.new("UIAspectRatioConstraint")
-		local UIAspectRatioConstraint_2 = Instance.new("UIAspectRatioConstraint")
-		
-		local sound_Effect = Instance.new("Sound", Example)
-		sound_Effect.SoundId = 'rbxassetid://9077482766'
-		sound_Effect.PlaybackSpeed = 2.5
-		sound_Effect.Volume = 0.1
-		sound_Effect:Play()
+		notify_lib.notify_count = 0
+	end
 
-		Example.Name = "Example"
-		Example.Parent = notify_List
-		Example.BackgroundColor3 = Color3.fromRGB(31, 31, 31)
-		Example.BackgroundTransparency = 1
-		Example.BorderColor3 = Color3.fromRGB(0, 0, 0)
-		Example.BorderSizePixel = 0
-		Example.Position = UDim2.new(0.254966736, 0, 0, 0)
-		Example.Size = UDim2.new(0.62932235, 0, 0.0854922757, 0)
+	local background = Instance.new("Frame")
+	local UICorner = Instance.new("UICorner")
+	local title = Instance.new("TextLabel")
+	local line = Instance.new("Frame")
+	local UICorner_2 = Instance.new("UICorner")
+	local icon = Instance.new("ImageLabel")
+	local UIStroke = Instance.new("UIStroke")
+	local UIScale = Instance.new("UIScale")
 
-		UICorner.CornerRadius = UDim.new(0, 9)
-		UICorner.Parent = Example
+	background.Name = "background"
+	background.Parent = notify_lib.notify.notify_list
+	background.AnchorPoint = Vector2.new(0.5, 0.5)
+	background.BackgroundColor3 = Color3.fromRGB(10, 12, 13)
+	background.BorderColor3 = Color3.fromRGB(0, 0, 0)
+	background.BorderSizePixel = 0
+	background.Position = UDim2.new(0, 0, 0, 0)
+	background.Size = UDim2.new(0, 130, 0, 33)
 
-		Data.Name = "Data"
-		Data.Parent = Example
+	UICorner.CornerRadius = UDim.new(0.3, 0)
+	UICorner.Parent = background
 
-		Icon.Name = "Icon"
-		Icon.Parent = Data
-		Icon.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
-		Icon.BackgroundTransparency = 1.000
-		Icon.BorderColor3 = Color3.fromRGB(0, 0, 0)
-		Icon.BorderSizePixel = 0
-		Icon.Position = UDim2.new(0.0373533294, 0, 0.168862924, 0)
-		Icon.Size = UDim2.new(0.0907369405, 0, 0.629204273, 0)
-		Icon.ZIndex = 6
-		Icon.Image = "rbxassetid://17745044731"
-		Icon.ImageColor3 = Color3.fromRGB(215, 215, 215)
-		Icon.ImageTransparency = 1
+	title.Name = "title"
+	title.Parent = background
+	title.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+	title.BackgroundTransparency = 1.000
+	title.BorderColor3 = Color3.fromRGB(0, 0, 0)
+	title.BorderSizePixel = 0
+	title.Position = UDim2.new(0, 32, 0, 6)
+	title.Size = UDim2.new(0, 80, 0, 21)
+	title.Font = Enum.Font.GothamBold
+	title.Text = text
+	title.TextColor3 = Color3.fromRGB(255, 255, 255)
+	title.TextSize = 16.000
+	title.TextWrapped = true
+	title.TextTransparency = 1
 
-		Text.Name = "Text"
-		Text.Parent = Data
-		Text.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
-		Text.BackgroundTransparency = 1.000
-		Text.BorderColor3 = Color3.fromRGB(0, 0, 0)
-		Text.BorderSizePixel = 0
-		Text.Position = UDim2.new(0.193315268, 0, -3.04304126e-06, 0)
-		Text.Size = UDim2.new(0.80398488, 0, 1.00000322, 0)
-		Text.ZIndex = 5
-		Text.Font = Enum.Font.GothamBold
-		Text.Text = notify_Text
-		Text.TextColor3 = Color3.fromRGB(215, 215, 215)
-		Text.TextSize = 21.000
-		Text.TextTransparency = 1
-		Text.TextWrapped = true
-		Text.TextXAlignment = Enum.TextXAlignment.Left
+	UIStroke.Parent = background
+	UIStroke.Color = Color3.fromRGB(48, 48, 48)
+	UIStroke.Transparency = 0.180
 
-		UIGradient.Transparency = NumberSequence.new{NumberSequenceKeypoint.new(0.00, 0.00), NumberSequenceKeypoint.new(0.65, 0.45), NumberSequenceKeypoint.new(1.00, 1.00)}
-		UIGradient.Parent = Text
+	UIScale.Parent = background
+	UIScale.Scale = 2
 
-		Line.Name = "Line"
-		Line.Parent = Example
-		Line.BackgroundColor3 = Color3.fromRGB(113, 155, 180)
-		Line.BackgroundTransparency = 1
-		Line.BorderColor3 = Color3.fromRGB(0, 0, 0)
-		Line.BorderSizePixel = 0
-		Line.Position = UDim2.new(0.16014117, 0, -1.51757968e-06, 0)
-		Line.Size = UDim2.new(0.00361005589, 0, 1.00000381, 0)
+	local text_size = TextService:GetTextSize(title.Text, title.TextSize, title.Font, Vector2.new(1000, title.TextSize))
 
-		UIAspectRatioConstraint.Parent = Line
-		UIAspectRatioConstraint.AspectRatio = 0.025
+	TweenService:Create(title, TweenInfo.new(5, Enum.EasingStyle.Exponential), {
+		TextTransparency = 0
+	}):Play()
 
-		UIAspectRatioConstraint_2.Parent = Example
-		UIAspectRatioConstraint_2.AspectRatio = 0.01
-		UIAspectRatioConstraint_2.DominantAxis = Enum.DominantAxis.Height
+	TweenService:Create(title, TweenInfo.new(0.4, Enum.EasingStyle.Exponential), {
+		Size = UDim2.new(0, text_size.X, 0, 21)
+	}):Play()
 
-		TweenService:Create(UIAspectRatioConstraint_2, TweenInfo.new(0.65, Enum.EasingStyle.Exponential, Enum.EasingDirection.InOut), {
-			AspectRatio = 7.000
+	TweenService:Create(background, TweenInfo.new(2, Enum.EasingStyle.Exponential), {
+		Size = UDim2.new(0, text_size.X + 65, 0, 33)
+	}):Play()
+
+	TweenService:Create(UIScale, TweenInfo.new(2, Enum.EasingStyle.Exponential), {
+		Scale = 1.3
+	}):Play()
+
+	task.delay(draw_time, function()
+		notify_lib.notify_count -= 1
+
+		TweenService:Create(background, TweenInfo.new(1.1, Enum.EasingStyle.Exponential), {
+			Position = UDim2.new(0, 0, 40, 0)
 		}):Play()
 
-		TweenService:Create(Example, TweenInfo.new(0.65, Enum.EasingStyle.Exponential, Enum.EasingDirection.InOut), {
-			BackgroundTransparency = 0.250
+		TweenService:Create(title, TweenInfo.new(1, Enum.EasingStyle.Exponential), {
+			TextTransparency = 1
 		}):Play()
 
-		TweenService:Create(Icon, TweenInfo.new(0.55, Enum.EasingStyle.Exponential, Enum.EasingDirection.InOut), {
-			ImageTransparency = 0.450
+		TweenService:Create(UIScale, TweenInfo.new(1, Enum.EasingStyle.Exponential), {
+			Scale = 1.2
 		}):Play()
 
-		TweenService:Create(Text, TweenInfo.new(1.25, Enum.EasingStyle.Exponential, Enum.EasingDirection.InOut), {
-			TextTransparency = 0.4
-		}):Play()
-
-		TweenService:Create(Line, TweenInfo.new(0.55, Enum.EasingStyle.Exponential, Enum.EasingDirection.InOut), {
-			BackgroundTransparency = 0.7
-		}):Play()
-
-		task.delay(on_screen_Time, function()
-			TweenService:Create(UIAspectRatioConstraint_2, TweenInfo.new(0.65, Enum.EasingStyle.Exponential, Enum.EasingDirection.InOut), {
-				AspectRatio = 0.01
-			}):Play()
-
-			TweenService:Create(Example, TweenInfo.new(0.65, Enum.EasingStyle.Exponential, Enum.EasingDirection.InOut), {
-				BackgroundTransparency = 1
-			}):Play()
-
-			TweenService:Create(Icon, TweenInfo.new(0.55, Enum.EasingStyle.Exponential, Enum.EasingDirection.InOut), {
-				ImageTransparency = 1
-			}):Play()
-
-			TweenService:Create(Text, TweenInfo.new(0.55, Enum.EasingStyle.Exponential, Enum.EasingDirection.InOut), {
-				TextTransparency = 1
-			}):Play()
-
-			TweenService:Create(Line, TweenInfo.new(0.55, Enum.EasingStyle.Exponential, Enum.EasingDirection.InOut), {
-				BackgroundTransparency = 1
-			}):Play()
-
-			task.delay(on_screen_Time + 1, function()
-				game:GetService("Debris"):AddItem(Example, 0)
-			end)
-		end)
+		Debris:AddItem(background, 1)
 	end)
 end
 
-return notify
+return notify_lib
