78c8611c7ba7b912cf996aea1b604fbc3ef60818
TheCtkHoster <danirusachith@gmail.com>
2024-07-14 21:17:54 +0530

    Update nurysium_ui.lua
    remove goofy ahh girl background image

--- nurysium_ui.lua
+++ nurysium_ui.lua
@@ -169,36 +169,6 @@ function nurysium: init(name: string, is_draggable: boolean, parent)
 
 	ui.Name = 'nurysium'
 	ui.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
-	
-	local girl_Image = Instance.new("Frame")
-	local girl = Instance.new("ImageLabel")
-	local girl_UIGradient = Instance.new("UIGradient")
-	local girl_UIAspectRatioConstraint = Instance.new("UIAspectRatioConstraint")
-
-	girl_Image.Name = "girl_Image"
-	girl_Image.Parent = ui
-	girl_Image.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
-	girl_Image.BackgroundTransparency = 1.000
-	girl_Image.BorderColor3 = Color3.fromRGB(0, 0, 0)
-	girl_Image.BorderSizePixel = 0
-	girl_Image.Size = UDim2.new(1, 0, 1, 0)
-
-	girl.Name = "girl"
-	girl.Parent = girl_Image
-	girl.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
-	girl.BackgroundTransparency = 1.000
-	girl.BorderColor3 = Color3.fromRGB(0, 0, 0)
-	girl.BorderSizePixel = 0
-	girl.Position = UDim2.new(0.730189502, 0, 0.475565195, 0)
-	girl.Size = UDim2.new(0.269810468, 0, 0.524434745, 0)
-	girl.Image = "rbxassetid://17663188480"
-
-	girl_UIGradient.Rotation = -74
-	girl_UIGradient.Transparency = NumberSequence.new{NumberSequenceKeypoint.new(0.00, 1.00), NumberSequenceKeypoint.new(0.17, 0.80), NumberSequenceKeypoint.new(0.34, 0.47), NumberSequenceKeypoint.new(0.43, 0.00), NumberSequenceKeypoint.new(0.50, 0.00), NumberSequenceKeypoint.new(0.68, 0.00), NumberSequenceKeypoint.new(0.90, 0.97), NumberSequenceKeypoint.new(1.00, 1.00)}
-	girl_UIGradient.Parent = girl
-	
-	girl_UIAspectRatioConstraint.Parent = girl
-	girl_UIAspectRatioConstraint.AspectRatio = 0.87
 
 	Background.Name = "Background"
 	Background.Parent = ui
